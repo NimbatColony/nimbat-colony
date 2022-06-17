13647a39f95212ee20d08926c83437fa300d9453
Nimbat Colony <nimbatcolony@github.com>
2022-06-16 17:30:20 -0700

    press some buttons !

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
-
+  @Output() toggleEvent = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleSidenav(): void {
+    this.toggleEvent.emit();
+  }
 }
