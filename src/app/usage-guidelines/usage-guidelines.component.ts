13a0bbb55d3efb75784fbf985c779e0b9cb587fc
Nimbat Colony <nimbatcolony@github.com>
2022-07-11 21:41:49 -0700

    make finn the fancy thing

--- src/app/usage-guidelines/usage-guidelines.component.ts
+++ src/app/usage-guidelines/usage-guidelines.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usage-guidelines.component.scss']
 })
 export class UsageGuidelinesComponent implements OnInit {
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  onMouseMove(elementName: string, e: any) {
+    const element = document.querySelector("#" + elementName);
+    if (element) {
+      element.setAttribute("style", `background-position-x: ${-e.layerX}px;background-position-y: ${-e.layerY}px`);
+      console.log(`${-e.clientX}px`);
+      console.log(e);
+    }
+  }
 }
